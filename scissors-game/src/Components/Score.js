fb17422ce5dfe70145c95d559d1d6040dae69373
lavinia_esp <lavinialoreda@gmail.com>
2022-07-10 13:08:42 +0200

    create player component

--- scissors-game/src/Components/Score.js
+++ scissors-game/src/Components/Score.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 
 const Score = (player) => {
 
-    const [player1Score, setPlayer1Score] = useState
+    const [player1Score, setPlayer1Score] = useState(0);
+    const [player2Score, setPlayer2Score] = useState(0);
 
     return (
         <>
