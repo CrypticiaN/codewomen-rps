d78b0f01c9caed655b8c8a5eebe555c98497b822
lavinia_esp <lavinialoreda@gmail.com>
2022-07-10 13:06:50 +0200

    Co-authored-by: Thais Costa <thais_costa31@hotmail.com> Co-authored-by: HelenJD <CrypticiaN@users.noreply.github.com> Co-authored-by: Hanna Huzava <anutaguzova@users.noreply.github.com>

---
+++ scissors-game/src/Components/Score.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Score = (player) => {
+
+    const [player1Score, setPlayer1Score] = useState
+
+    return (
+        <>
+            <h3></h3>
+        </>
+    );
+};
+
+export default Score;
